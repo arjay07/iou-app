634c08c768bf4d568f8cd24c53fe6f3697d1a676
leandroyabut <leandro.yabut@smoothstack.com>
2021-07-03 20:04:40 -0600

    Add health check route.

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, HttpCode, Res } from '@nestjs/common';
+import { Response } from 'express';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('/health')
+  healthCheck(@Res() res: Response) {
+    res.sendStatus(200);
   }
+  
 }
