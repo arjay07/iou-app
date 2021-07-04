0fb8717d9c42e506bb1bbe406da18adf1fa638f1
leandroyabut <leandro.yabut@smoothstack.com>
2021-07-04 04:36:20 -0600

    Add password hashing to user registration.

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { RegisterUserDto } from 'src/dto/user/register-user-dto';
 import { UserService } from './user.service';
 
 @Controller('users')
@@ -9,6 +10,11 @@ export class UserController {
     @Get('/:username')
     getUserByUsername(@Param('username') username: string) {
         return this.userService.getUserByUsername(username);
+    }
+
+    @Post()
+    registerUser(@Body() registerUserDto: RegisterUserDto) {
+        return this.userService.registerUser(registerUserDto);
     }
 
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,12 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { RegisterUserDto } from 'src/dto/user/register-user-dto';
 import { User } from 'src/entity/user/user.entity';
 import { getRepository } from 'typeorm';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UserService {
 
     async getUserByUsername(username: string) {
         return await getRepository(User).findOne(username);
+    }
+
+    async registerUser(registerUserDto: RegisterUserDto) {
+        const saltRounds = 10;
+        const hashed = await bcrypt.hash(registerUserDto.password, saltRounds)
+        registerUserDto.password = hashed;
+        return await getRepository(User).save(registerUserDto);
     }
 
 }
