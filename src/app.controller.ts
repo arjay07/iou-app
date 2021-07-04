8b15a3099df6cc8d5b545f9d2130dd8feba56b82
leandroyabut <leandro.yabut@smoothstack.com>
2021-07-03 20:06:25 -0600

    Update eslint to allow empty functions.

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,14 +1,12 @@
-import { Controller, Get, HttpCode, Res } from '@nestjs/common';
+import { Controller, Get, Res } from '@nestjs/common';
 import { Response } from 'express';
-import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor() {}
 
   @Get('/health')
   healthCheck(@Res() res: Response) {
     res.sendStatus(200);
   }
-  
 }
